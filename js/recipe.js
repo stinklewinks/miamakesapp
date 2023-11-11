4ebac953f22db45a1630296e53f222383c0c6398
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2023-11-11 01:05:27 -0600

    added beginnings of Recipe class

---
+++ js/recipe.js
@@ -0,0 +1,20 @@
+class Recipe {
+    static name = 'Recipe name';
+    static description = 'Recipe Description';
+
+    constructor(){
+        return `${this.name} is created: ${this.description}`;
+    }
+
+    changeRecipeName(newName){
+        this.name = newName;
+    }
+
+    viewRecipeName(){
+        return this.name;
+    }
+
+    viewDescription(){
+        return this.description;
+    }
+}
