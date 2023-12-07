5185a16a5d75231f9e36725e2c9852651f737e68
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2023-12-07 13:14:01 -0600

    nay break this

--- front/source/src/admin/post.ts
+++ front/source/src/admin/post.ts
@@ -1,7 +1,7 @@
 class Post {
-    private name!: string;
+    private title!: string;
     private author!: string;
-    private descruption!: string;
+    private description!: string;
     private slug!: string;
     private content!: string;
     private tags= <string[]>[];
@@ -9,6 +9,90 @@ class Post {
     private likes!: number;
     private shares!: number;
     private category!: string;
+
+    __constructor(title: string, author: string){
+        this.title = title;
+        this.author = author;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+
+    setTitle(title: string){
+        this.title = title;
+    }
+
+    getAuthor(){
+        return this.author;
+    }
+
+    setAuthor(author: string){
+        this.author = author;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+
+    setDescription(desc: string){
+        this.description = desc;
+    }
+
+    getSlug(){
+        return this.slug;
+    }
+
+    setSlug(slug: string){
+        this.slug = slug;
+    }
+
+    getContent(){
+        return this.content;
+    }
+
+    setContent(content: string){
+        this.content = content;
+    }
+
+    getComments(){
+        return this.comments;
+    }
+
+    addComment(comment: string){
+        this.comments.push(comment);
+    }
+
+    getTages(){
+        return this.tags;
+    }
+
+    addTag(tag: string){
+        this.tags.push(tag);
+    }
+
+    getLikes(){
+        return this.likes;
+    }
+
+    addLike(like:number){
+        this.likes += like;
+    }
+
+    getShares(){
+        return this.shares;
+    }
+
+    addShare(share: number){
+        this.shares += share;
+    }
+
+    getCategory(){
+        return this.category;
+    }
+    setCategory(cat: string){
+        this.category = cat;
+    }
 
 }
 
