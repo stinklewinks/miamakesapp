12974f82ab492b936e2203ae764af9ef8b48c385
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2023-12-06 22:10:46 -0600

    Added front end Post class

--- front/source/src/admin/post.ts
+++ front/source/src/admin/post.ts
@@ -1,6 +1,15 @@
 class Post {
     private name!: string;
+    private author!: string;
+    private descruption!: string;
+    private slug!: string;
+    private content!: string;
+    private tags= <string[]>[];
+    private comments = <string[]>[];
+    private likes!: number;
+    private shares!: number;
+    private category!: string;
 
 }
 
-export {}
+export {Post}
